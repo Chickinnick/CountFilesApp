fb1b0673161dfa62233ce81388286ae7e61b2717
chickinnick <chickinnick@gmail.com>
2015-05-14 12:25:11 +0300

    Initial commit

---
+++ src/com/company/FileCounter.java
@@ -0,0 +1,94 @@
+package com.company;
+
+
+import java.io.*;
+import java.util.Formatter;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.Vector;
+import java.util.logging.Level;
+
+public class FileCounter implements Runnable {
+    int sequenceNomber = 0;
+    String sourceFile;
+    String outputFile;
+
+    public FileCounter(String sourceFile,String outputFile) {
+
+        this.sourceFile = sourceFile;
+        this.outputFile = outputFile;
+    }
+
+
+
+    public Set<File> readFromFile(File file) {
+
+        Set<File> directories = new LinkedHashSet<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                directories.add(new File(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Main.logger.log(Level.WARNING, "Error while reading file");
+        }
+        return directories;
+    }
+
+    public void writeToFile(Vector<ResultOfCounting> results, String outputFile) {
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(new File(outputFile));
+            for (ResultOfCounting item : results) {
+                fileWriter.write(item.toString() + "\n");
+                displayResults(item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Vector<ResultOfCounting> countAllElements(Set<File> set) {
+        Vector<ResultOfCounting> results = new Vector<>();
+        for (File item : set) {
+            Main.logger.log(Level.INFO, "search in" + item.getAbsolutePath() + " - START");
+            results.add(countItems(item, new ResultOfCounting(item.getAbsolutePath())));
+        }
+
+        return results;
+    }
+
+    private ResultOfCounting countItems(File item, ResultOfCounting resultOfCounting) {
+
+        for (File f : item.listFiles()) {
+            resultOfCounting.incrementNumberOfFiles();
+
+            if (f.isDirectory())
+                countItems(f, resultOfCounting);
+        }
+        return resultOfCounting;
+    }
+
+    public void displayResults(ResultOfCounting resultOfCounting) {
+
+        Formatter formatter = new Formatter();
+        System.out.println(formatter.format("|%5d|%5d|%40s|", ++sequenceNomber, resultOfCounting.getNumberOfFiles(), resultOfCounting.getDirectory()));
+
+    }
+
+
+
+    @Override
+    public void run() {
+        Set<File> setOfDirs = readFromFile(new File(sourceFile));
+        Vector<ResultOfCounting> results = countAllElements(setOfDirs);
+        writeToFile(results, outputFile);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Main implements Runnable{
+
+    public static final Logger logger = java.util.logging.Logger.getLogger("CounterLogger");
+
+
+    public static void main(String[] args) {
+
+        new Thread(new Main()).start();
+
+        String sourceFile = args[0];
+        String outputFile = args[1];
+
+        new Thread(new FileCounter(sourceFile, outputFile)).start();
+
+    }
+
+    @Override
+    public void run() {
+        int characterCode =0;
+        while (true) {
+            try {
+                if ((characterCode = System.in.read()) == 'q') {
+                    System.out.println("break");
+
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
